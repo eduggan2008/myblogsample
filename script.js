b2f7fdadea3c98c3846883fd11714dbb232b0cfe
Edward Duggan <eduggan2008@gmail.com>
2020-06-13 10:32:22 -0400

    finished js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function showPosts() {
     posts.forEach(post => {
         const postEl = document.createElement('div');
         postEl.classList.add('post');
-        postEl.innetHTML = `
+        postEl.innerHTML = `
             <div class="number">${post.id}</div>
             <div class="post-info">
                 <h2 class="post-title">${post.title}</h2>
@@ -33,8 +33,50 @@ async function showPosts() {
     });
 }
 
+// Show loader and fetch more posts
+function showLoading() {
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
+    }, 1000);
+}
+
+// Filter posts by input
+function filterPosts(e) {
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach(post => {
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+        if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+            post.style.display = 'flex';
+        } else {
+            post.style.display = 'none';
+        }
+    });
+}
+
+
 // Show initial posts
 showPosts();
 
 // Event Listener
+window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        showLoading();
+    } else {
+
+    }
+});
+
+filter.addEventListener('input', filterPosts);
